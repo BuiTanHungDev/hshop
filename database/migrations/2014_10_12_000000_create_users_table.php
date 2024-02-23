36cdd2a6ac4e09f2a2b56c0524ca866534da24b3
BuiTanHungDev <hungnlk2002@gmai.com>
2024-02-23 21:43:44 +0700

    update table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,7 @@ class CreateUsersTable extends Migration
             $table->string('provider')->nullable();
             $table->string('provider_id')->nullable();
             $table->enum('status',['active','inactive'])->default('active');
+            
             $table->rememberToken()->nullable();
             $table->timestamps();
         });
